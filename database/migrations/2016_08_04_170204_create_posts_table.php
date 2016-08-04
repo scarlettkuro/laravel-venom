2a7905771510ef94d2154493338566f22711bca8
scarlettkuro <spider.kom@gmail.com>
2016-08-04 20:11:07 +0300

    init db schema

---
+++ database/migrations/2016_08_04_170204_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text', 2000);
+            $table->boolean('private')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
